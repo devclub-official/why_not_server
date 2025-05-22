b1ea82d56a293114719a7b63f1a0cb8ec6a6d25e
jonathan970813 <jonathan813@naver.com>
2025-05-22 17:36:51 +0900

    init project

---
+++ alarmeet-api/src/test/java/click/alarmeet/alarmeetapi/AlarmeetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package click.alarmeet.alarmeetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmeetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
