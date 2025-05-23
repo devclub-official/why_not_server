14e43b725aa23e1dc2b491668f58bbc6c8c84f4e
jonathan970813 <jonathan813@naver.com>
2025-05-24 00:42:37 +0900

    feat(api): user domain

---
+++ alarmeet-api/src/main/java/click/alarmeet/alarmeetapi/apis/users/domain/User.java
@@ -0,0 +1,19 @@
+package click.alarmeet.alarmeetapi.apis.users.domain;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Document(collection = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class User {
+	@Id
+	private ObjectId id;
+}
