14e22fae7a330e73aa5f3a886aad0e26df911802
jonathan970813 <jonathan813@naver.com>
2025-05-24 14:49:32 +0900

    docs: getGroups

--- alarmeet-api/src/main/java/click/alarmeet/alarmeetapi/apis/groups/api/GroupApi.java
+++ alarmeet-api/src/main/java/click/alarmeet/alarmeetapi/apis/groups/api/GroupApi.java
@@ -1,10 +1,15 @@
 package click.alarmeet.alarmeetapi.apis.groups.api;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import click.alarmeet.alarmeetapi.apis.groups.dto.GroupCreateDto;
+import click.alarmeet.alarmeetapi.apis.groups.dto.GroupListDto.GroupListRes;
+import click.alarmeet.alarmeetapi.common.response.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,7 +19,14 @@ public interface GroupApi {
 	@Operation(summary = "그룹 생성 api")
 	@ApiResponses(value = {
 		@ApiResponse(responseCode = "201", description = "그룹 생성"),
-		@ApiResponse(responseCode = "409", description = "그룹 수 제한 초과")
+		@ApiResponse(responseCode = "409", description = "그룹 수 제한 초과", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
 	})
-	void createGroup(@RequestBody @Validated GroupCreateDto.GroupCreateReq group);
+	ResponseEntity<?> createGroup(@RequestBody @Validated GroupCreateDto.GroupCreateReq group);
+
+	@Operation(summary = "그룹 조회 api")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "유저가 참여한 그룹들 반환"),
+		@ApiResponse(responseCode = "404", description = "유저 존재하지 않을 떄", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+	})
+	ResponseEntity<GroupListRes> getGroups();
 }
