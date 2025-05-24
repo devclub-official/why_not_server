6e3a71c3d96aa7690bef3e6644a6fd4fab5e3d57
jonathan970813 <jonathan813@naver.com>
2025-05-24 22:28:21 +0900

    refactor(api): change group path id name to groupId

--- alarmeet-api/src/main/java/click/alarmeet/alarmeetapi/apis/groups/controller/GroupController.java
+++ alarmeet-api/src/main/java/click/alarmeet/alarmeetapi/apis/groups/controller/GroupController.java
@@ -45,22 +45,22 @@ public class GroupController implements GroupApi {
 	}
 
 	@Override
-	@GetMapping("/{id}")
-	public ResponseEntity<GroupDetailRes> getGroup(@PathVariable ObjectId id) {
-		return ResponseEntity.ok(groupUseCase.getGroup(id, TEST_ID));
+	@GetMapping("/{groupId}")
+	public ResponseEntity<GroupDetailRes> getGroup(@PathVariable ObjectId groupId) {
+		return ResponseEntity.ok(groupUseCase.getGroup(groupId, TEST_ID));
 	}
 
 	@Override
-	@PutMapping("/{id}")
-	public ResponseEntity<?> updateGroup(@PathVariable ObjectId id, @RequestBody @Validated GroupUpdateReq group) {
-		groupUseCase.updateGroup(id, TEST_ID, group);
+	@PutMapping("/{groupId}")
+	public ResponseEntity<?> updateGroup(@PathVariable ObjectId groupId, @RequestBody @Validated GroupUpdateReq group) {
+		groupUseCase.updateGroup(groupId, TEST_ID, group);
 		return ResponseEntity.noContent().build();
 	}
 
 	@Override
-	@DeleteMapping("/{id}")
-	public ResponseEntity<?> deleteGroup(@PathVariable ObjectId id) {
-		groupUseCase.deleteGroup(id, TEST_ID);
+	@DeleteMapping("/{groupId}")
+	public ResponseEntity<?> deleteGroup(@PathVariable ObjectId groupId) {
+		groupUseCase.deleteGroup(groupId, TEST_ID);
 
 		return ResponseEntity.noContent().build();
 	}
